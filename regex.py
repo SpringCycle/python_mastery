f02abc179ca9c36bb35d3807a4794ecaf46cb076
VANDANA R <vandana@VANDANAs-MacBook-Air.local>
2024-02-18 17:01:42 +0530

    adding regex-file

--- regex.py
+++ regex.py
@@ -1,17 +1,58 @@
-#regex-findall pattern
-import re
+# #regex-findall pattern
+# import re
 
-text = "The quick brown fox"
-pattern = r"brown"
+# text = "The quick brown fox"
+# pattern = r"brown"
 
-search = re.search(pattern, text)
-if search:
-    print("Pattern found:", search.group())
-
-else:
-    print("pattern not found")
+# search = re.search(pattern, text)
+# if search:
+#     print("Pattern found:", search.group())
+# else:
+#     print("pattern not found")
 # ====================================================
+# #regex-match pattern
+# import re 
+# text = "The quick brown fox"
+# pattern = r"brown"
 
-#regex-match pattern
+# match = re.match(text,pattern)
+# if match:
+#     print("Match found",match.group())
+# else:
+#     print("Match not found")
 
-#import re 
+# ========================================================
+
+# #regex-replace
+
+# line= "this is my brown fox"
+# word= "is"
+# replacement = "your"
+
+# replace= re.sub(pattern,replacement,word)
+# print("Word",replace)
+
+# =========================================
+
+# # to find word in a line
+
+# line = "Hello Python"
+# word= "Python"
+
+# result = re.search(line,word)
+# if result:
+#     print("Word found",search.group())
+# else:
+#     print("Word isn't found")
+# ==============================================
+
+# # split the line
+
+# import re
+# text = "apple,mango,orange,banana"
+# pattern= r","
+
+# split_result= re.split(pattern,text)
+# print("Split",split_result)
+
+# # =================================================
